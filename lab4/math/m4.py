b5e72a08e4ffdd2bc1b34ab8095b9bea430ddbc1
Asan0348 <a_abuov@kbtu.kz>
2024-06-27 15:40:33 +0500

    Create m4.py

---
+++ lab4/math/m4.py
@@ -0,0 +1,12 @@
+def comp(base, height):
+    area = base * height
+    return area
+
+
+base_length = 5
+height = 6
+
+
+area = comp(base_length, height)
+
+print(f"Площадь параллелограмма с основанием {base_length} и высотой {height}: {area}")
